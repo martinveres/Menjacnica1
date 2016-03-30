317fca8380f68c64469226a66bd9b5eee7cbe8a3
Martin Veres <martin.veres95gmail.com>
2016-03-30 02:27:18 +0200

    Dodata klasa menjacnica koja implementira MenjacnicaInterfejs, metode su prazne, postoji samo njihovo telo.

---
+++ Menjacnica1/src/menjacnica/Menjacnica.java
@@ -0,0 +1,28 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica.interfejs.MenjacnicaInterfejs;
+import menjacnica.kurs.Kurs;
+
+public class Menjacnica implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKursZaNekiDan(Kurs kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKursZaNekiDan(GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs vratiKursZaNekiDan(GregorianCalendar dan) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
