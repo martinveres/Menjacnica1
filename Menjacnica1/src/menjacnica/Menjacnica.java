631721cb9f2e1020959a0a418d43e4ab1023ed1e
Martin Veres <martin.veres95gmail.com>
2016-03-30 03:08:26 +0200

    Uradjena klasa Menjacnica, na drugaciji nacin.

--- Menjacnica1/src/menjacnica/Menjacnica.java
+++ Menjacnica1/src/menjacnica/Menjacnica.java
@@ -1,41 +1,39 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnica.interfejs.MenjacnicaInterfejs;
 import menjacnica.kurs.Kurs;
 import menjacnica.Valute;
 public class Menjacnica implements MenjacnicaInterfejs {
-	private Valute[] valute = new Valute[1000]; 
-	private int brojValuta = 0;
+	private LinkedList<Kurs> kursevi = new LinkedList<Kurs>();
+	
+	
 	@Override
 	public void dodajKursZaNekiDan(Kurs kurs) {
-		valute[brojValuta].dodajKurs(kurs);;
+		kursevi.add(kurs);
 		
 	}
 
 	@Override
-	public void obrisiKursZaNekiDan(Kurs kurs) {
-		for(int i=0;i<brojValuta;i++) {
-			for (int j = 0; j < valute[i].getBrojKurseva(); j++) {
-				if(kurs.equals(valute[i].getKursevi()[j])) {
-					valute[i].getKursevi()[j]=null;
-				}
+	public void obrisiKursZaNekiDan(Kurs dan) {
+		for (int i = 0; i < kursevi.size(); i++) {
+			if(kursevi.get(i).equals(dan)) {
+				kursevi.remove(i);
 			}
 		}
 		
 	}
 
 	@Override
-	public Kurs vratiKursZaNekiDan(Kurs kurs) {
-		for(int i=0;i<brojValuta;i++) {
-			for (int j = 0; j < valute[i].getBrojKurseva(); j++) {
-				if(kurs.equals(valute[i].getKursevi()[j])) {
-					return valute[i].getKursevi()[j];
-				}
+	public Kurs vratiKursZaNekiDan(Kurs dan) {
+		for (int i = 0; i < kursevi.size(); i++) {
+			if(kursevi.get(i).equals(dan)) {
+				return kursevi.get(i);
 			}
 		}
 		return null;
 	}
-
+	
 }
