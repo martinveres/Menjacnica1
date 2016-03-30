0bec8da6129dcd8014c6a9bbe61f2d48ea1c8856
Martin Veres <martin.veres95gmail.com>
2016-03-30 02:11:22 +0200

    Dodat atribut datum u klasi Kurs, geter i seter za njega, metode toString i equals.

--- Menjacnica1/src/menjacnica/Valute.java
+++ Menjacnica1/src/menjacnica/Valute.java
@@ -5,7 +5,8 @@ import menjacnica.kurs.Kurs;
 public class Valute {
 	private String ime;
 	private String skracenica;
-	private Kurs[] kurs = new Kurs[365];
+	private Kurs[] kursevi = new Kurs[365];
+	private int brojKurseva=0;
 	public String getIme() {
 		return ime;
 	}
@@ -18,11 +19,30 @@ public class Valute {
 	public void setSkracenica(String skracenica) {
 		this.skracenica = skracenica;
 	}
-	public Kurs[] getKurs() {
-		return kurs;
+	public Kurs[] getKursevi() {
+		return kursevi;
 	}
-	public void setKurs(Kurs[] kurs) {
-		this.kurs = kurs;
+	public void setKurs(Kurs[] kurs, int brojKurseva) {
+		this.kursevi = kurs;
+		this.brojKurseva = brojKurseva;
+	}
+	@Override
+	public String toString() {
+		
+		String s = ime + " " + skracenica + " ";
+		for(int i = 0;i<brojKurseva; i++) {
+			s+= kursevi[i].toString();
+		}
+		return s;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Valute) {
+			if(((Valute) obj).getIme().equals(getIme())) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 }
--- Menjacnica1/src/menjacnica/kurs/Kurs.java
+++ Menjacnica1/src/menjacnica/kurs/Kurs.java
@@ -1,6 +1,9 @@
 package menjacnica.kurs;
 
+import java.util.GregorianCalendar;
+
 public class Kurs {
+	GregorianCalendar datum;
 	private double kupovni;
 	private double prodajni;
 	private double srednji;
@@ -21,5 +24,29 @@ public class Kurs {
 	}
 	public void setSrednji(double srednji) {
 		this.srednji = srednji;
+	}
+	
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	@Override
+	public String toString() {
+		return "" + datum.get(GregorianCalendar.YEAR) + " " +   datum.get(GregorianCalendar.MONTH) + " "
+	+  datum.get(GregorianCalendar.DAY_OF_WEEK) + " "+ kupovni + " " + " " + srednji + " " + prodajni;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Kurs) {
+			if((((Kurs) obj).getDatum().get(GregorianCalendar.YEAR)==getDatum().get(GregorianCalendar.YEAR))&&
+					((Kurs) obj).getDatum().get(GregorianCalendar.MONTH)==getDatum().get(GregorianCalendar.MONTH)
+					&&((Kurs) obj).getDatum().get(GregorianCalendar.DAY_OF_WEEK)==getDatum().get(GregorianCalendar.DAY_OF_WEEK)) {
+					 
+				return true;
+			}
+		}
+		return false;
 	}
 }
